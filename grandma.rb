f0e58858e09501a55088f910464c3de9baafbb4f
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 03:38:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,8 @@ def speak_to_grandma(phrase)
   is phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   elseif phrase == phrase.upcase 
-  return
+  return "NO, NOT SINCE 1938!"
+  else phrase == phrase.downcase
+  return "HUH?! SPEAK UP, SONNY!"
 end
 end
