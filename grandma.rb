5de262d38cbcf72174bb89acff0763a4aeb08b60
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 02:19:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 input_phrase = "Hi grandma, how are you?"
 def speak_to_grandma(phrase)
-  if phrase != 
+  if phrase != phrase.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
