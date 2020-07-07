15a9a38f81044cf3165ff85f2df85535bf4e917f
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 02:05:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,12 +12,3 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-love = "I LOVE YOU GRANDMA!"
-
-if (.upcase)
- puts "NO, NOT SINCE 1938!"
-elsif love = .upcase
-  puts "I LOVE YOU TOO PUMPKIN!"
-else
-  puts "HUH?! SPEAK UP, SONNY!"
-end
