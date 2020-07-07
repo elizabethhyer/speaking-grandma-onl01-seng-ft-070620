1dfe1f7f63490bca3be26ae43060f06289fbab8e
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 01:39:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase)
-  puts "#{Hello, Grandma!}"
+def speak_to_grandma(hello grandma)
   
 end 
