051a472df7f7ee9eeee3fa878b0d6eff4606d182
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 02:38:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,20 +12,21 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-input_phrase_upcase = "Hi grandma, how are you?".upcase
-input_love_upcase = "i love you grandma!".upcase
-input_phrase_downcase = "Hi grandma, how are you?".downcase
 
 def speak_to_grandma(phrase_upcase, love_upcase, phrase_downcase)
-  if (phrase_upcase)
-    puts "NO, NOT SINCE 1938!"
-  elsif (love_upcase)
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else (phrase_downcase)
-    puts "HUH?! SPEAK UP, SONNY!"
-  end
-end 
+if answer == "Hi grandma, how are you?".upcase
+  puts "NO, NOT SINCE 1938!"
+  puts phrase_upcase
+elsif answer == "Hi grandma, how are you?".downcase
+  puts ""HUH?! SPEAK UP, SONNY!""
+  puts phrase_downcase
+elsif answer == "i love you grandma!".upcase
+  puts "I LOVE YOU TOO PUMPKIN!"
+  puts love.upcase
+elsif answer == "no"
+  puts "Okay then...."
+end
 
-speak_to_grandma(input_phrase_downcase)
-speak_to_grandma(input_phrase_upcase)
-speak_to_grandma(input_love_upcase)
+speak_to_grandma(phrase_downcase)
+speak_to_grandma(phrase_upcase)
+speak_to_grandma(love_upcase)
